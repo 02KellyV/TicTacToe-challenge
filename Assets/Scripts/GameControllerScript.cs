a9aa15c06187fbeb99dab9b873457179df8a5d1e
02KellyV <900@holbertonschool.com>
2020-05-08 15:31:29 -0500

    first commit

---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControllerScript : MonoBehaviour
+{
+    public Text[] buttonList;
+    private string playerSide;
+
+    void Awake()
+    {
+        SetGameControllerReferenceOnButton();
+        playerSide = "X";
+    }
+
+    void SetGameControllerReferenceOnButton()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpaceScript  >().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+        ChangeSides();
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+    }
+
+    void ChangeSides()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+    }
+}
---
+++ Assets/Scripts/GridSpaceScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpaceScript : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+
+    private GameControllerScript gameController;
+
+    // Start is called before the first frame update
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+
+    public void SetGameControllerReference(GameControllerScript controller)
+    {
+        gameController = controller;
+    }
+}
