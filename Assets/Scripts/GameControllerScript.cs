18244f2de7e671283b666d376c072fd7ef01c5c0
02KellyV <900@holbertonschool.com>
2020-05-08 18:32:00 -0500

    terminado sin colores

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -1,22 +1,53 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+
+[System.Serializable]
+public class PlayerColor
+{
+    public Color panelColor;
+    public Color textColor;
+}
+
+[System.Serializable]
+public class Player
+{
+    public Image panel;
+    public Text text;
+}
+
 
 public class GameControllerScript : MonoBehaviour
 {
     public Text[] buttonList;
     private string playerSide;
 
+    public GameObject gameOverPanel;
+    public Text gamerOverText;
+
+    private int moveCount;
+
+    public GameObject restarButton;
+
+    public Player playerX;
+    public Player playerO;
+    public PlayerColor activePlayerColor;
+    public PlayerColor inactivePlayerColor;
+
     void Awake()
     {
+        gameOverPanel.SetActive(false);
         SetGameControllerReferenceOnButton();
         playerSide = "X";
+        moveCount = 0;
+        SetPlayerColors(playerX, playerO);
     }
 
     void SetGameControllerReferenceOnButton()
     {
         for (int i = 0; i < buttonList.Length; i++)
         {
-            buttonList[i].GetComponentInParent<GridSpaceScript  >().SetGameControllerReference(this);
+            buttonList[i].GetComponentInParent<GridSpaceScript>().SetGameControllerReference(this);
         }
     }
 
@@ -27,6 +58,8 @@ public class GameControllerScript : MonoBehaviour
 
     public void EndTurn()
     {
+        moveCount++;
+
         if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
         {
             GameOver();
@@ -59,6 +92,12 @@ public class GameControllerScript : MonoBehaviour
         {
             GameOver();
         }
+
+        if(moveCount >= 9)
+        {
+            SetGameOver("It's a draw!");
+        }
+
         ChangeSides();
     }
 
@@ -66,12 +105,59 @@ public class GameControllerScript : MonoBehaviour
     {
         for (int i = 0; i < buttonList.Length; i++)
         {
-            buttonList[i].GetComponentInParent<Button>().interactable = false;
+            SetBoardInteractable(false);
         }
+
+        SetGameOver(playerSide + " Wins!");
     }
 
     void ChangeSides()
     {
         playerSide = (playerSide == "X") ? "O" : "X";
+
+        if(playerSide == "X")
+        {
+            SetPlayerColors(playerX, playerO);
+        }else
+        {
+            SetPlayerColors(playerO, playerX);
+        }
+    }
+
+    void SetGameOver(string value)
+    {
+        gameOverPanel.SetActive(true);
+        gamerOverText.text = value;
+    }
+
+    public void RestarGame()
+    {
+        playerSide = "X";
+        moveCount = 0;
+        gameOverPanel.SetActive(false);
+
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            SetBoardInteractable(true);
+            buttonList[i].text = "";
+        }
+
+        SetPlayerColors(playerX, playerO);
+    }
+
+    void SetBoardInteractable(bool toggle)
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = toggle;
+        }
+    }
+
+    void SetPlayerColors(Player newPlayer, Player oldPlayer)
+    {
+        newPlayer.panel.color = activePlayerColor.panelColor;
+        newPlayer.text.color = activePlayerColor.textColor;
+        oldPlayer.panel.color = inactivePlayerColor.panelColor;
+        oldPlayer.text.color = inactivePlayerColor.textColor;
     }
 }
